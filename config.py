48300b84ab2fff7376d5c9715070bb19ccb038f0
sudeepnew123 <sudeepmandal8999@gmail.com>
2025-05-27 09:30:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))  # Fallback to 0 if not set
-
+BOT_USERNAME = "@Typing_speedbot"  # @ hata ke sirf username
 API_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
